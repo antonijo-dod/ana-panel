c90c9884c8823a97af35e54233659f1d7876ee9f
antonijo-dod <a.djodj@gmail.com>
2026-01-10 12:35:54 +0000

    fix url

--- src/hooks/api/useCoolifyGetAplicationLogs.tsx
+++ src/hooks/api/useCoolifyGetAplicationLogs.tsx
@@ -6,7 +6,7 @@ const URL = import.meta.env.VITE_COOLIFY_API
 export const coolifyApplicationLogsQueryOptions = (id: string) => queryOptions({
     queryKey: ['coolifyApplicationLogs', id],
     queryFn: async () => {
-        const response = await axiosInstance.get(`${URL}/applications/${id}/logs`)
+        const response = await axiosInstance.get(`${URL}/api/v1/applications/${id}/logs`)
         if (response.status !== 200) {
             throw new Error('Network response was not ok')
         }
